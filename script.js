887c4e04f6707cdce4405b4b96216af86732dd70
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2021-08-04 11:13:19 +0200

    project finished

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const nav__white = document.querySelector(".nav__white");
 const navigation = document.querySelector(".navigation");
 const toggleBtn = document.querySelector("#toggleBtn");
 const transformedElements = document.querySelectorAll(".nav");
+const goUppBtn = document.querySelector(".goUpBtn");
+const header = document.querySelector(".header");
 
 // NAVIGATION  TOGGLE
 toggleBtn.addEventListener("click", () => {
@@ -17,10 +19,15 @@ toggleBtn.addEventListener("click", () => {
   }
 });
 
-// NAVIGATION fixed
 window.addEventListener("scroll", () => {
   nav__white.style.top = `${navigation.clientHeight - window.scrollY}px`;
+  if (scrollY > header.clientHeight) {
+    goUppBtn.style.display = "block";
+  } else {
+    goUppBtn.style.display = "none";
+  }
 
+  // NAVIGATION fixed
   if (scrollY > nav__white.clientHeight) {
     transformedElements.forEach((el) => el.classList.remove("visible"));
     toggleBtn.className = "navigation__toggleBtn fas fa-bars";
@@ -32,4 +39,23 @@ window.addEventListener("hashchange", () => {
   document.querySelector("body").style.overflow = "visible";
   transformedElements.forEach((el) => el.classList.remove("visible"));
   toggleBtn.className = "navigation__toggleBtn fas fa-bars";
+});
+
+// Show more photos
+
+const showMoreBtn = document.querySelector("#showMoreBtn");
+const galleryContainerAdditinal = document.querySelector(
+  ".gallery__container--additional"
+);
+const galleryContainer = document.querySelector(".gallery__container");
+
+showMoreBtn.addEventListener("click", () => {
+  galleryContainerAdditinal.classList.toggle("show");
+  galleryContainer.insertAdjacentElement("beforeend", showMoreBtn);
+  if (galleryContainerAdditinal.classList.contains("show")) {
+    showMoreBtn.textContent = `Pokaż mniej \u2191`;
+  } else {
+    showMoreBtn.textContent = "Pokaż więcej \u2193";
+    galleryContainer.scrollIntoView();
+  }
 });
